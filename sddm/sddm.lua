a0b16318c471fcbccee7c299c44dad7b15ba1e76
GwynnN7 <mattcheru03@gmail.com>
2026-07-08 11:13:31 +0200

    Laptop update

--- sddm/sddm.lua
+++ sddm/sddm.lua
@@ -1,16 +1,22 @@
 hl.monitor({
     output   = "DP-1",
     mode     = "1920x1080@165",
-    position = "0x0",
+    position = "auto",
+    scale    = "1",
+})
+
+hl.monitor({
+    output   = "eDP-1",
+    mode     = "1920x1080@60",
+    position = "auto",
     scale    = "1",
 })
 
 hl.monitor({
     output   = "HDMI-A-1",
     mode     = "1920x1080@60",
-    position = "1920x0",
+    position = "auto",
     scale    = "1",
-    mirror   = "DP-1"
 })
 
 hl.config({
